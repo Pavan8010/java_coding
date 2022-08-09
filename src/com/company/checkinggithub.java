7c747976c07800725db739ac1e32f40df57308a2
Pavan Pabitwar <2020bcs129@sggs.ac.in>
2022-08-09 23:22:13 +0530

    cheking git hub

---
+++ src/com/company/checkinggithub.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class checkinggithub {
+        public static void main(String[] args) {
+            System.out.println("cheking git hub is working properly or not");
+        }
+}
