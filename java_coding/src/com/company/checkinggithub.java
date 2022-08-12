a281d4caf2aa55d21976b94fb3ca7c5a00ac4b37
Pavan8010 <2020bcs129@sggs.ac.in>
2022-08-12 13:28:36 +0530

    commit mesage

--- java_coding/src/com/company/checkinggithub.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class checkinggithub {
-        public static void main(String[] args) {
-            System.out.println("cheking git hub is working properly or not");
-        }
-}
